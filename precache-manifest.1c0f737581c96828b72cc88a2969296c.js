f4a48faa610e7aef0a9fbff8968e2bafb8ce0f10
dmobley0608 <67826887+dmobley0608@users.noreply.github.com>
2020-09-15 18:43:04 -0400

    Updates

---
+++ precache-manifest.1c0f737581c96828b72cc88a2969296c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593e95cfe00390d1fee54d37fd86ebcc",
+    "url": "/familyhub/index.html"
+  },
+  {
+    "revision": "5e8934e87711a0ed3022",
+    "url": "/familyhub/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "d34403e99d93e0ca775a",
+    "url": "/familyhub/static/css/main.4cffcb40.chunk.css"
+  },
+  {
+    "revision": "5e8934e87711a0ed3022",
+    "url": "/familyhub/static/js/2.1d538af7.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/familyhub/static/js/2.1d538af7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34403e99d93e0ca775a",
+    "url": "/familyhub/static/js/main.5139760d.chunk.js"
+  },
+  {
+    "revision": "8df4f41cb71452661980",
+    "url": "/familyhub/static/js/runtime-main.69876d0d.js"
+  },
+  {
+    "revision": "02fd1f31873612bf0230a04ece8d8143",
+    "url": "/familyhub/static/media/hallows.02fd1f31.ico"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/familyhub/precache-manifest.2890cfe0bd32afdf9f54d1702512820e.js"
+  "/familyhub/precache-manifest.1c0f737581c96828b72cc88a2969296c.js"
 );
 
 self.addEventListener('message', (event) => {
